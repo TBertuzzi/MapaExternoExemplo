913e0a1e8659d0228620c833179bf4bc8968e156
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-02-26 08:38:32 -0300

    :tada: Subindo exemplo ao github

---
+++ MapaExternoExemplo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.ExternalMaps;
+using Xamarin.Forms;
+
+namespace MapaExternoExemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Handle_Clicked(object sender, EventArgs e)
+        {
+            // var success = await CrossExternalMaps.Current.NavigateTo("NESS Processos e Tecnologia", -23.6008793, -46.6950054);
+
+            var success = await CrossExternalMaps.Current.NavigateTo("NESS Processos e Tecnologia",
+             "Rua Doutor Geraldo Campos Moreira 240", "São Paulo", "São Paulo", "04533-085", "Brasil", "");
+
+        }
+    }
+}
